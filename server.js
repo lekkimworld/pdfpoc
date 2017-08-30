244e379ab75a64420cf3378c893cf61b64c82848
Mikkel Flindt Heisterberg <mheisterberg@salesforce.com>
2017-08-30 21:11:22 +0200

    Read data from Salesforce for table

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express');
+var session = require('express-session');
 var env = require('node-env-file');
 var dateFormat = require('dateformat');
 var oauth2 = require('salesforce-oauth2');
+var rp = require('request-promise');
 const PdfPrinter = require('pdfmake');
 const pdfFonts = {
     Roboto: {
@@ -11,8 +13,13 @@ const pdfFonts = {
         bolditalics: 'fonts/Roboto-MediumItalic.ttf'
     }
 };
+const sess = {
+    secret: 'keyboard cat',
+    cookie: {}
+}
 
 var app = express();
+app.use(session(sess));
 var port = process.env.PORT || 8080;
 
 // Load environment variables for localhost
@@ -35,6 +42,13 @@ app.set('view engine', 'ejs');
 
 // add routes
 app.get('/', function(req, res) {
+    // see if there is an access token already
+    if (req.session.access_token) {
+        res.redirect('/print');
+        return;
+    }
+
+    // initiate oauth flow
     var uri = oauth2.getAuthorizationUrl({
         redirect_uri: callbackUrl,
         client_id: consumerKey,
@@ -45,101 +59,101 @@ app.get('/', function(req, res) {
 });
 app.get('/oauth/callback', function(req, res) {
     let authorizationCode = req.query.code;
-    console.log(`Auth code <${authorizationCode}`);
     let args = {
         "redirect_uri": callbackUrl,
         "client_id": consumerKey,
         "client_secret": consumerSecret,
-        "code": authorizationCode
+        "code": authorizationCode,
+        "base_url": baseUrl
     };
     oauth2.authenticate(args, function(err, payload) {
         if (err) {
             res.send(JSON.stringify(err));
             return;
         }
-        console.log(payload);
-        const instance_url = payload.instance_url;
-        res.write(`received data: ${instance_url}`);
+        const {access_token, instance_url} = payload;
+        req.session.access_token = access_token;
+        req.session.instance_url = instance_url;
+        res.redirect('/print');
     });
 })
 app.get('/print', function(req, res) {
-    const printer = new PdfPrinter(pdfFonts);
-    const generatedDate = dateFormat(new Date(), 'yyyy-mm-dd HH:MM');
-    const docDefinition1 = { content: 'This is an sample PDF printed with pdfMake' };
-    const docDefinition2 = {
-        content: [
-            {text: 'Working Group List', style: 'header'},
-            {text: `Generate on ${generatedDate}`, alignment: 'right'},
-            {
-                style: 'tableExample',
-                table: {
-                    widths: [100, '*', 200, '*'],
-                    dontBreakRows: true,
-                    body: [
-                        [{text: 'Some Table Header\nthat goes across two lines', colSpan: 4, fillColor: '#ff0000', alignment: 'center'},{},{},{}],
-                        ['width=100', 'star-sized', 'width=200', 'star-sized'],
-                        ['fixed-width cells have exactly the specified width', {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}],
-                        ['fixed-width cells have exactly the specified width', {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}],
-                        ['fixed-width cells have exactly the specified width', {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}],
-                        ['fixed-width cells have exactly the specified width', {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}],
-                        ['fixed-width cells have exactly the specified width', {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}],
-                        ['fixed-width cells have exactly the specified width', {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}],
-                        ['fixed-width cells have exactly the specified width', {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}],
-                        ['fixed-width cells have exactly the specified width', {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}],
-                        ['fixed-width cells have exactly the specified width', {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}],
-                        ['fixed-width cells have exactly the specified width', {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}]
-                    ]
-                }
-            },
-            {
-                style: 'tableExample',
-                table: {
-                    widths: [100, '*', 200, '*'],
-                    dontBreakRows: true,
-                    body: [
-                        [{text: 'Some Table Header\nthat goes across two lines', colSpan: 4, fillColor: '#ff0000', alignment: 'center'},{},{},{}],
-                        ['width=100', 'star-sized', 'width=200', 'star-sized'],
-                        ['fixed-width cells have exactly the specified width', {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}],
-                        ['fixed-width cells have exactly the specified width', {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}],
-                        ['fixed-width cells have exactly the specified width', {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}],
-                        ['fixed-width cells have exactly the specified width', {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}],
-                        ['fixed-width cells have exactly the specified width', {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}],
-                        ['fixed-width cells have exactly the specified width', {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}],
-                        ['fixed-width cells have exactly the specified width', {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}],
-                        ['fixed-width cells have exactly the specified width', {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}],
-                        ['fixed-width cells have exactly the specified width', {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}],
-                        ['fixed-width cells have exactly the specified width', {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}, {text: 'nothing interesting here', italics: true, color: 'gray'}]
-                    ]
-                }
-            }
-        ],
-        styles: {
-            header: {
-                fontSize: 18,
-                bold: true,
-                margin: [0, 0, 0, 10]
-            },
-            subheader: {
-                fontSize: 16,
-                bold: true,
-                margin: [0, 10, 0, 5]
-            },
-            tableExample: {
-                margin: [0, 5, 0, 15]
-            },
-            tableHeader: {
-                bold: true,
-                fontSize: 13,
-                color: 'black'
-            }
-        },
-        defaultStyle: {
-            // alignment: 'justify'
-        }
+    // make sure we have an access token
+    if (!req.session.access_token) {
+        res.redirect('/');
+        return;
     }
-    const pdf = printer.createPdfKitDocument(docDefinition2);
-    pdf.pipe(res);
-    pdf.end();
+
+    // use Salesforce API to get users
+    const options = {
+        uri: `${req.session.instance_url}/services/data/v39.0/query/?q=SELECT+Id,+Firstname,+Lastname,+Email+FROM+User`,
+        headers: {
+            "Authorization": `Bearer ${req.session.access_token}`,
+            "Content-Type": "application/json"
+        }
+    };
+    rp(options).then(strdata => {
+        // received data from Salesforce - build data
+        const bodySF = [[{text: 'Salesforce.com Emails', style: 'tableHeader', colSpan: 4}, {}, {}, {}]];
+        const bodyNonSF = [[{text: 'Other Emails', style: 'tableHeader', colSpan: 4}, {}, {}, {}]];
+        const data = JSON.parse(strdata);
+        Object.keys(data.records).forEach(k => {
+            const u = data.records[k];
+            if (u.Email.indexOf('salesforce.com') >= 0) {
+                bodySF.push([u.Id, u.Email, u.FirstName, u.LastName]);
+            } else {
+                bodyNonSF.push([u.Id, u.Email, u.FirstName, u.LastName]);
+            }
+        })
+
+        // build definition for PDF
+        const docDefinition = {
+            content: [],
+            styles: {
+                header: {
+                    fontSize: 18,
+                    bold: true,
+                    margin: [0, 0, 0, 10]
+                },
+                tableExample: {
+                    margin: [0, 5, 0, 15]
+                },
+                tableHeader: {
+                    bold: true,
+                    fontSize: 13,
+                    color: 'black',
+                    alignment: 'center'
+                }
+            }
+        }
+        docDefinition.content.push({text: 'Working Group List', style: 'header'});
+        docDefinition.content.push({text: `Generate on ${dateFormat(new Date(), 'yyyy-mm-dd HH:MM')}`, alignment: 'right'});
+        docDefinition.content.push({
+            style: 'tableExample',
+            table: {
+                widths: [100, 100, '*', '*'],
+                dontBreakRows: true,
+                body: bodySF
+            }
+        });
+        docDefinition.content.push({
+            style: 'tableExample',
+            table: {
+                widths: [100, 100, '*', '*'],
+                dontBreakRows: true,
+                body: bodyNonSF
+            }
+        });
+        
+        // create PDF
+        const printer = new PdfPrinter(pdfFonts);
+        const pdf = printer.createPdfKitDocument(docDefinition);
+        pdf.pipe(res);
+        pdf.end();
+
+    }).error(err => {
+        res.send(err);
+    });
 });
 
 // Serve your app
